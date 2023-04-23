09fe6bd2994bc7e5a8f9918f6aac9d556a242d89
AtWillB <46000428+AtWillB@users.noreply.github.com>
2023-04-23 18:07:36 -0400

    testing github instead

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-/*Put helper functions here*/
+async function get_vis_data(url){
+  results = await fetch(url);
+  return results;
+}
+
 
 
 async function mainEvent() {
   // the async keyword means we can make API requests
-  const loadDataButton = document.querySelector("#data_load");
+  // const loadDataButton = document.querySelector("#data_load");
 
   
 
-  // Example function for yoinking data from
-  loadDataButton.addEventListener("click", async (submitEvent) => {
+  // Example function for yoinking data froms
+  // loadDataButton.addEventListener("click", async (submitEvent) => {
     // async has to be declared on every function that needs to "await" something
     console.log("loading data");
-    loadAnimation.style.display = "inline-block";
+    // loadAnimation.style.display = "inline-block";
 
 
-    const results = await fetch(
-      "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"
-    );
-
-    const storedList = await results.json();
-    localStorage.setItem("storedData", JSON.stringify(storedList));
-    pasrsedData = storedList;
-
-
-    console.table(storedList);
-  });
+    results  = await fetch("https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+DISTINCT discoverymethod, count(discoverymethod)+from+ps+groupby+discoverymethod&format=json");
+    console.log(results);
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
