e17b5a3c453763f93830d561b8a6ff2f956bf94d
AtWillB <46000428+AtWillB@users.noreply.github.com>
2023-05-02 08:53:24 -0400

    made retrieve_from_local

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
+// Load data into local storage
 async function load_to_local() {
   disc_vis = await fetch_json("https://exo-dash-planets.vercel.app/api/vis/discovery_methods_bar");
   stell_vis = await fetch_json("https://exo-dash-planets.vercel.app/api/vis/stellar_type_bar");
+  num_planets = await fetch_json("https://exo-dash-planets.vercel.app/api/stat/num_planets");
+
 
   localStorage.setItem('disc_vis', JSON.stringify(disc_vis));
   localStorage.setItem('stell_vis', JSON.stringify(stell_vis));
-  
+  localStorage.setItem('num_planets', JSON.stringify(num_planets));
 }
 
+// retrieve data from localstorage
+function retrieve_from_local(item) {
+  result = "";
+
+  if (item == "disc_vis") {
+    result = localStorage.getItem("disc_vis");
+    result = JSON.parse(result);
+  }
+  else if (item == "stell_vis") {
+    result = localStorage.getItem("stell_vis");
+    result = JSON.parse(result);
+  }
+
+  else if (item == "num_planets") {
+    result = localStorage.getItem("num_planets");
+    result = JSON.parse(result);
+  }
+
+  return result
+}
+
+// short function for grabbing json from fetch request
 async function fetch_json(url) {
   results = await fetch(url);
   results = await results.json();
   return results;
 }
 
+//function to make the chart
 async function create_chart(results) {
   console.log(results);
 
@@ -35,8 +61,7 @@ async function create_chart(results) {
 async function mainEvent() {
   // the async keyword means we can make API requests
   const dropdownButton = document.querySelector("#vis_selector");
-  const disc_chart = document.querySelector("#discovery_method");
-  const stellar_chart = document.querySelector("#stellar_type");
+
 
 
   
@@ -44,18 +69,13 @@ async function mainEvent() {
 
   console.log("loading data");
   load_to_local(); // load data into local storage
-  
-  disc_vis = localStorage.getItem("disc_vis");
-  disc_vis = JSON.parse(disc_vis);
-  stell_vis = localStorage.getItem("stell_vis");
-  stell_vis = JSON.parse(stell_vis);
 
-
-
+  const disc_vis = retrieve_from_local('disc_vis');
+  const stell_vis = retrieve_from_local('stell_vis');
 
   chart = await create_chart(disc_vis)
 
-
+  // Drop down button changes the visualization
   dropdownButton.addEventListener("change", async (event) => {
     if (dropdownButton.value == "disc") {
         chart.config.data.datasets[0].data = disc_vis.Y;
@@ -72,6 +92,8 @@ async function mainEvent() {
 
     
   })
+
+  // 
 
     
 }
